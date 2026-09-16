4188fe8bbaea7ac748a137b245b48bc9cdabbfd9
s08055822764-collab <s08055822764@gmail.com>
2026-09-16 21:56:19 +0900

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "opioid-calculator-test-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(cacheName) {
+            return cacheName !== CACHE_NAME;
+          })
+          .map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then(function(networkResponse) {
+        return networkResponse;
+      }).catch(function() {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
